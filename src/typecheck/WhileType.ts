51f1df2c5b5913655ee2c81c4c5d822d26083a0e
Max Alegre Defaut <max@biller.uy>
2017-09-14 20:20:22 -0300

    WhileType implementado

--- src/typecheck/WhileType.ts
+++ src/typecheck/WhileType.ts
@@ -1,3 +1,23 @@
 export class WhileType {
-		
- }
+    private static isCreating: Boolean = false;
+    static instance: WhileType = null;
+
+    constructor () {
+        if (!WhileType.isCreating) throw new Error("This is a real singleton. Get an instance via WhileType.getInsance();");
+    }
+
+    static getInstance(): WhileType {
+        if (WhileType.instance === null) {
+            WhileType.isCreating = true;
+            WhileType.instance = new WhileType();
+            WhileType.isCreating = false;
+        }
+
+        return WhileType.instance;
+    }
+
+    public isCompatible(): Boolean {
+        return false;
+    }
+
+}
